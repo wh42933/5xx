4701198d26fbbc15fac1a65efe20f4dcb094b98e
wh42933 <wh42933@ravens.racc.edu>
2018-04-09 16:44:39 -0400

    final push to update and correct work in branch

--- README.md
+++ README.md
@@ -34,3 +34,30 @@ inside out project 5xx - STEP-501.0 | Branch 501-0
  -    Awards
             h2: What makes me proud...
             content: Awards: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.
+			
+			  inside out project 5xx - STEP-502 | iop-me Updates
+     me WP Site Changes
+          - added and activated a new theme
+               - WP Bootstrap Starter
+                    - Version: 3.0.11 
+                    - By Afterimage Designs
+          - added the following pages to the me wp site
+          - welcome to me (nested the following pages below this one)
+               - home
+                    - copied content from header in to this page
+               - about, experience, education, skills, interests, awards, contact
+                    - copied the content for each of the pages from the static site
+                         - "<p>-from me</p>"added to each page to allow for easy validation of successful API calls
+               - no content added to the "welcome to me" page - used as parent for API calls
+        
+inside out project 5xx - STEP-502.0 | Branch 502-0
+     README.md file changes
+          - prepended the title to the production site link
+          - added link to GitHub Pages to the top of the content
+     - index.html
+          - removed comments and cleaned code
+          - added main element and nested all section elements inside of it
+          - added link to fontawesome to bottom of boddy for cog in splash screen
+          - added link to app.js to bottom of boddy created in this step
+          - app.js
+          
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,55 @@
-$("body").prepend('<div class=text-white style=position:fixed;top:0;bottom:0;width:100%;z-index:99999999;padding-top:12%;background-color:rgba(87,87,87,.95);" id="cover-page"><div id="cover-page-con" class="container text-center"style="opacity:100%;"><img alt="Brian Savage"class="img-fluid rounded-circle"src=http://via.placeholder.com/150x150><h1>William Harding</h1><p class=lead>...inspired innovative technology leader & follower.<p class=lead style=font-size:80%><i class="fa-1x fa-cog fa-spin fas"></i> we are preparing your experiance now...</div></div>');
-		2.	var parentPageID = 55;
-		3.	window.onload = getContent();
-		4.	
-		5.	function getContent() {
-		6.		$.ajax({
-		7.			method: 'GET',
-		8.			url: 'https://me.inside-out-project.com/wp-json/wp/v2/pages/?parent=' + parentPageID + '&order=asc&orderby=menu_order',
-		9.			dataType: 'json',
-		10.			success: function (data) {
-		11.				if (data.length > 0) {
-		12.					var thePage = '';
-		13.					var theMenu = '<ul class="navbar-nav ml-auto">';
-		14.					var className = '';
-		15.					data.forEach(function (item) {
-		16.						if (item.slug === 'home') {
-		17.							$("header").html(item.content.rendered);
-		18.						} else {
-		19.							let isActive = (theMenu === null) ? ' active' : '';
-		20.							theMenu = theMenu + '<li class="nav-item"> <a class="nav-link js-scroll-trigger' + isActive + '" href="#' + item.slug + '">' + item.title.rendered + '</a> </li>';
-		21.							className = (className === '') ? ' class="bg-light"' : '';
-		22.							thePage = thePage + '<section id="' + item.slug + '"' + className + '>' + item.content.rendered + '</section>';
-		23.						}
-		24.					});
-		25.					$("#navbarResponsive").html(theMenu + '</ul>');
-		26.					$("main").html(thePage);
-		27.				}
-		28.			},
-		29.			error: function () {
-		30.				console.log('bad');
-		31.			}
-		32.		});
-		33.		setTimeout(function () {
-		34.			$("#cover-page").fadeOut(2000);
-		35.			startScroll();
-		36.			resizeDiv();
-		37.		}, 2000);
-		38.	}
-		39.	window.onresize = function (event) {
-		40.		resizeDiv();
-		41.	}
-		42.	
-		43.	function resizeDiv() {
-		44.		let vpw = $(window).width();
-		45.		let vph = $(window).height();
-		46.		let navH = $('nav').height();
-		47.		let footerH = $('nav').height();
-		48.		let setvph = vph - navH;
-		49.		$("section,header").css({
-		50.			"height": setvph
-		51.		});
-		52.		$("header").css({
-		53.			"margin-top": navH
-		54.		});
-		55.	}
+$("body").prepend('<div class=text-white style=position:fixed;top:0;bottom:0;width:100%;z-index:99999999;padding-top:12%;background-color:rgba(87,87,87,.95);" id="cover-page"><div id="cover-page-con" class="container text-center"style="opacity:100%;"><img alt="William Harding"class="img-fluid rounded-circle"src=http://via.placeholder.com/150x150><h1>William Harding</h1><p class=lead>...inspired innovative technology leader & follower.<p class=lead style=font-size:80%><i class="fa-1x fa-cog fa-spin fas"></i> we are preparing your experiance now...</div></div>');
+var parentPageID = 75;
+window.onload = getContent();
+
+function getContent() {
+	$.ajax({
+		method: 'GET',
+		url: 'https://me.n3vmt.com/wp-json/wp/v2/pages/?parent=' + parentPageID + '&order=asc&orderby=menu_order',
+		dataType: 'json',
+		success: function (data) {
+			if (data.length > 0) {
+				var thePage = '';
+				var theMenu = '<ul class="navbar-nav ml-auto">';
+				var className = '';
+				data.forEach(function (item) {
+					if (item.slug === 'home') {
+						$("header").html(item.content.rendered);
+					} else {
+						let isActive = (theMenu === null) ? ' active' : '';
+						theMenu = theMenu + '<li class="nav-item"> <a class="nav-link js-scroll-trigger' + isActive + '" href="#' + item.slug + '">' + item.title.rendered + '</a> </li>';
+						className = (className === '') ? ' class="bg-light"' : '';
+						thePage = thePage + '<section id="' + item.slug + '"' + className + '>' + item.content.rendered + '</section>';
+					}
+				});
+				$("#navbarResponsive").html(theMenu + '</ul>');
+				$("main").html(thePage);
+			}
+		},
+		error: function () {
+			console.log('bad');
+		}
+	});
+	setTimeout(function () {
+		$("#cover-page").fadeOut(2000);
+		startScroll();
+		resizeDiv();
+	}, 2000);
+}
+window.onresize = function (event) {
+	resizeDiv();
+}
+
+function resizeDiv() {
+	let vpw = $(window).width();
+	let vph = $(window).height();
+	let navH = $('nav').height();
+	let footerH = $('nav').height();
+	let setvph = vph - navH;
+	$("section,header").css({
+		"height": setvph
+	});
+	$("header").css({
+		"margin-top": navH
+	});
+}
